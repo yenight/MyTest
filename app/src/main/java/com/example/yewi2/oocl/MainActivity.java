8d83075d4e2aaa67c11e4717ce5ee9c32c0a7916
YEWI2 <1227061087@qq.com>
2018-08-28 16:16:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yewi2/oocl/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.yewi2.oocl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
